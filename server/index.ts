009ed5a1a08531687f14e98f294c62cc2f374942
just4n4cc <just4n4cc@yandex.ru>
2021-09-19 22:52:52 +0300

    Сделал дефолтный сервер

---
+++ server/index.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import body from "body-parser"
+import morgan from "morgan";
+import cookie from "cookie-parser";
+
+const app = express();
+app.use(body.json())
+app.use(morgan('dev'))
+app.use(express.static("./public"));
+app.use(cookie());
+
+app.post('/login', (req,  res) => {
+//
+});
+
+app.post('/signup', (req,  res) => {
+//
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,  () => {
+    console.log(`Server listening port ${port}`);
+});
