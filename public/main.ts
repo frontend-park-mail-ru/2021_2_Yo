db769c9586aa89274b6e8dc9e9e90f6ba8e4cbe0
just4n4cc <just4n4cc@yandex.ru>
2021-09-21 21:29:31 +0300

    Обновился до BT-6

--- public/main.ts
+++ public/main.ts
@@ -1,5 +1,6 @@
 import HeaderComponent from "./components/Header/Header.js"
 import EventHeaderComponent from "./components/EventsHeader/EventsHeader.js"
+import FilterHeaderComponent from "./components/FilterHeader/FilterHeader.js"
 
 declare global {
     interface Window {
