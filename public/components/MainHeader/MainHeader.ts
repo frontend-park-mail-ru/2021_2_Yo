b8d10d9bf7121b8cb5cbfc6d01ef9be980c88fab
just4n4cc <just4n4cc@yandex.ru>
2021-09-26 01:19:42 +0300

    Настроил отображение MainPage в зависимости от того, авторизован ли юзер, причесал css, настроил под heroku

--- public/components/MainHeader/MainHeader.ts
+++ public/components/MainHeader/MainHeader.ts
@@ -2,9 +2,9 @@ import { UserData } from "../../types.js";
 
 export default class MainHeaderComponent {
     #parent: HTMLElement
-    #user: UserData
+    #user: UserData | undefined
 
-    constructor(parent: HTMLElement, user: UserData) {
+    constructor(parent: HTMLElement, user?: UserData) {
         this.#parent = parent;
         this.#user = user;
     }
@@ -15,14 +15,23 @@ export default class MainHeaderComponent {
                 <div class="mhContent">
                     <div class="geo">
                         <img src="./img/logo.png">
-                        <img id="geoimg" src="./img/geo.png">
-                        <span class="imgCaption">{{geo}}</span>
+                        {{#if this}}
+                            <img id="geoimg" src="./img/geo.png">
+                            <span class="imgCaption">{{geo}}</span>
+                        {{/if}}
                     </div>
                     <input id="searchInput" type="text" placeholder="Поиск...">
-                    <div class="userBox">
-                        <img src="https://source.boringavatars.com/marble/60/{{name}}">
-                        <span class="imgCaption">{{name}}</span>
-                    </div>
+                    {{#if this}}
+                        <div class="userBox">
+                            <img src="https://source.boringavatars.com/marble/60/{{name}}">
+                            <span class="rightSideText">{{name}}</span>
+                        </div>
+                    {{else}}
+                        <div class="authBox">
+                            <a class="authAnchor" href="/login">Войти</a>
+                            <a class="authAnchor rightSideText" href="/signup">Регистрация</a>
+                        </div>
+                    {{/if}}
                 </div>
             </header>
         `;
