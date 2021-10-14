cec6f0f4ff595d0a876836c2d09daebaff34a2f3
just4n4cc <just4n4cc@yandex.ru>
2021-10-14 20:12:21 +0300

    status

--- public/components/MainPage/MainPage.ts
+++ public/components/MainPage/MainPage.ts
@@ -1,9 +1,11 @@
 import HeaderComponent from '../Header/Header.js';
 import { EventCardData, UserData } from '../../types.js'
 import EventBoardComponent from '../EventBoard/EventBoard.js';
+import SearchStatus from '../SearchStatus/SearchStatus.js';
 import EventHeaderComponent from '../EventHeader/EventHeader.js';
 import FilterHeaderComponent from '../FilterHeader/FilterHeader.js';
 import MainHeaderComponent from '../MainHeader/MainHeader.js';
+import SearchStatusComponent from '../SearchStatus/SearchStatus.js';
 
 export default class MainPageComponent {
     #parent: HTMLElement
@@ -21,12 +23,20 @@ export default class MainPageComponent {
         // const eHeader = new EventHeaderComponent(this.#parent);
         // const fHeader = new FilterHeaderComponent(this.#parent);
         const Header = new HeaderComponent(this.#parent, this.#user)
+        const status = {
+            question: 'Где потусить ',
+            when: 'сегодня',
+            link: ' в ',
+            city: 'Москве',
+        }
+        const SearchStatus = new SearchStatusComponent(this.#parent, status)
         const eBoard = new EventBoardComponent(this.#parent, this.#events);
 
         // mHeader.render();
         // eHeader.render();
         // fHeader.render();
         Header.render();
+        SearchStatus.render();
         eBoard.render();
     }
 }
---
+++ public/components/SearchStatus/SearchStatus.ts
@@ -0,0 +1,25 @@
+export default class SearchStatusComponent {
+    #parent: HTMLElement
+    #data: any
+
+    constructor(parent: HTMLElement, data: any) {
+        this.#parent = parent;
+        this.#data = data;
+    }
+
+    render() {
+        const source = `
+            {{#with this}}
+                <div class="status">
+                    <span>{{question}}</span>
+                    <span class="status__when">{{when}}</span>
+                    <span>{{link}}</span>
+                    <span class="status__city">{{city}}</span>
+                </div>
+            {{/with}}
+        `;
+        const template = window.Handlebars.compile(source);
+        this.#parent.innerHTML += template(this.#data);
+    }
+}
+
