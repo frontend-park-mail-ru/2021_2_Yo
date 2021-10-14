0cdb82f01c5eaa8c5030eae85a629a583710133d
Ksenia <ksju.nikitina@rambler.ru>
2021-10-14 17:21:09 +0300

    .

--- public/components/LoginPage&SignupPage/LoginPage.ts
+++ public/components/LoginPage&SignupPage/LoginPage.ts
@@ -92,6 +92,7 @@ export default class LoginPageComponent {
                         par.innerHTML += temp({error})
                     }
                 } else {
+                    par.classList.remove("input-block_error")
                     item.input.classList.remove("form-input_error")
                     item.input.classList.add("form-input_correct");
                     while (par.children.length !== 2) {
