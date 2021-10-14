c16f5f0401d82f4d0d8df81c0e17ee3b14c80e9b
just4n4cc <just4n4cc@yandex.ru>
2021-10-15 01:32:08 +0300

    event

--- public/components/EventBoard/EventBoard.ts
+++ public/components/EventBoard/EventBoard.ts
@@ -60,7 +60,6 @@ export default class EventBoardComponent {
                                     <span class="events__viewed-score">{{viewed}}</span>
                                 </div>
                             </div>
-                            
                         </div>
                     {{/each}}
                 </div>
