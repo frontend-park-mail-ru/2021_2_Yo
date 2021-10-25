45c39bf230ffec035c38ae973af8cdfd9d7c5dac
just4n4cc <just4n4cc@yandex.ru>
2021-10-25 22:51:09 +0300

    user test

--- public/modules/store/user.ts
+++ public/modules/store/user.ts
@@ -4,20 +4,27 @@ import Events from '../eventbus/events.js';
 import { fetchGet } from '../request/request.js';
 
 class UserStore {
-    #user?: UserData;
+    // Если undefined - надо сходить за данными
+    //      null - данные уже запрошены
+    //      UserData - данные сохранены, можно отдать сразу
+    #user: UserData | null | undefined;
 
     constructor() {
-        this.#userHandle();
         Bus.on(Events.UserReq, this.#userHandle);
         Bus.on(Events.UserLogout, this.#logoutHandle);
-        // Bus.emit(Events.UserReq);
+        Bus.emit(Events.UserReq);
     }
 
     #userHandle = (() => {
+        if (this.#user === null) {
+            return;
+        }
+
         if (this.#user) {
             Bus.emit(Events.UserRes, this.#user);
             console.log('stored:', this.#user);
         } else {
+            this.#user = null;
             void fetchGet(ApiUrls.User, 
                 (data: FetchResponseData) => {
                     const {status, json} = data;
@@ -26,10 +33,14 @@ class UserStore {
                             this.#user = {name: json.body.name, geo: 'Мытищи'};
                             Bus.emit(Events.UserRes, this.#user);
                             console.log('loaded:', this.#user);
+                            return;
                         }
                     } 
-                }
+                    this.#user = undefined;
+                },
+                () => this.#user = undefined
             );
+
         }
     }).bind(this);
 
