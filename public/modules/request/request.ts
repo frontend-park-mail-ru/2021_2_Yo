9ea715a00bf007cf206dfcded5997169a71170e9
just4n4cc <just4n4cc@yandex.ru>
2021-10-26 22:11:43 +0300

    new api test

--- public/modules/request/request.ts
+++ public/modules/request/request.ts
@@ -6,7 +6,8 @@ const METHODS = {
 };
 
 // const API = 'https://bmstusasa.herokuapp.com';
-const API = 'https://95.163.212.36:8081';
+const API = 'https://yobmstu.herokuapp.com/';
+// const API = 'https://95.163.212.36:8081';
 
 export function fetchGet(url: ApiUrls, callback?: (args?: any) => void, error?: (args?: any) => void) {
     let HTTPStatus: number;
