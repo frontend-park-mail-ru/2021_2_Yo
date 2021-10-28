cc00a6dba75072b274ea78e217fca779127d7bbf
Ksenia <ksju.nikitina@rambler.ru>
2021-10-28 23:33:55 +0300

    переезд на Степино апи

--- public/modules/request/request.ts
+++ public/modules/request/request.ts
@@ -6,8 +6,8 @@ const METHODS = {
     DELETE: 'DELETE',
 };
 
-// const API = 'https://bmstusasa.herokuapp.com';
-const API = 'https://yobmstu.herokuapp.com';
+const API = 'https://bmstusasa.herokuapp.com';
+// const API = 'https://yobmstu.herokuapp.com';
 // const API = 'https://95.163.212.36:8081';
 
 export function fetchGet(url: string, callback?: (args?: any) => void, error?: (args?: any) => void) {
