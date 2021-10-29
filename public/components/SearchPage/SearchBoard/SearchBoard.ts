dccf4785912be77a6f1b074ed1f78519293f346c
just4n4cc <just4n4cc@yandex.ru>
2021-10-29 23:09:36 +0300

    redirect to event

--- public/components/SearchPage/SearchBoard/SearchBoard.ts
+++ public/components/SearchPage/SearchBoard/SearchBoard.ts
@@ -1,4 +1,4 @@
-import { EventData } from '../../../types.js';
+import { EventData, UrlPathnames } from '../../../types.js';
 import Bus from '../../../modules/eventbus/eventbus.js';
 import Events from '../../../modules/eventbus/events.js';
 
@@ -11,31 +11,35 @@ export default class SearchBoard {
         Bus.on(Events.EventsError, this.#handleEventsError);
         Bus.emit(Events.EventsReq);
 
-        const event = `
-            <div class="event-li">
-                <div class="event-li__img bg-img-wrapper">
-                    <img class="bg-img" src="{{imgUrl}}">
-                </div>
-                <div class ="event-li__content">
-                    <span class="event-li__title">{{title}}</span>
-                    <div class="event-li__description">{{description}}</div>
-                    <div class="event-li__info event-li__description">
-                        <div class="event-li__description">
-                            <span>Когда:&nbsp;</span>
-                            <span class="text_date">{{date}}</span>
-                            <span>Где:&nbsp;</span>
-                            <span class="text_geo">{{geo}}</span>
-                        </div>
-                        <div class="event-li__viewed">
-                            <img class="event-li__viewed-img" src="/img/viewed2.png">
-                            <span>{{viewed}}</span>
+        const list = `
+            <div id="events-list">
+            {{#each events}}
+                <div class="event-li">
+                    <div class="event-li__img bg-img-wrapper">
+                        <img class="bg-img" src="{{imgUrl}}">
+                    </div>
+                    <div class ="event-li__content">
+                        <span id="event-{{@index}}" class="event-li__title" data-id="{{id}}">{{title}}</span>
+                        <div class="event-li__description">{{description}}</div>
+                        <div class="event-li__info event-li__description">
+                            <div class="event-li__description">
+                                <span>Когда:&nbsp;</span>
+                                <span class="text_date">{{date}}</span>
+                                <span>Где:&nbsp;</span>
+                                <span class="text_geo">{{geo}}</span>
+                            </div>
+                            <div class="event-li__viewed">
+                                <img class="event-li__viewed-img" src="/img/viewed2.png">
+                                <span>{{viewed}}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
+            {{/each}}
             </div>
         `;
 
-        window.Handlebars.registerPartial('event', event);
+        window.Handlebars.registerPartial('events-list', list);
     }
 
     #handleEvents = ((events: EventData[]) => {
@@ -54,21 +58,32 @@ export default class SearchBoard {
         const source = `
             <h2>Бекендеры решили ПОТУСИТЬ => API отъехало.</h2>
         `;
-        const list = <HTMLElement>document.getElementById('search-content');
-        list.innerHTML = window.Handlebars.compile(source)();
+        const content = <HTMLElement>document.getElementById('search-content');
+        content.innerHTML = window.Handlebars.compile(source)();
     }
 
     #renderEmpty() {
         const source = `
             <h2>Прости бро, нет таких ивентосов</h2>
         `;
-        const list = <HTMLElement>document.getElementById('search-content');
-        list.innerHTML = window.Handlebars.compile(source)();
+        const content = <HTMLElement>document.getElementById('search-content');
+        content.innerHTML = window.Handlebars.compile(source)();
     }
 
     #addListeners() {
-        
+        for (let i = 0;; i++) {
+            const event = <HTMLElement>document.getElementById('event-' + i.toString());
+            if (!event) break;
+
+            event.addEventListener('click', this.#handleEventClick);
+        }
     }
+
+    #handleEventClick = (e: MouseEvent) => {
+        const target = <HTMLElement>e.target;
+        const id = <string>target.dataset['id'];
+        Bus.emit(Events.RouteUrl, UrlPathnames.Event + '/' + id);
+    };
 
     render(events?: EventData[]) {
         const source = `
@@ -78,11 +93,7 @@ export default class SearchBoard {
             </div>
             <div id="search-content">
                 {{#if events}}
-                <div id="events-list">
-                        {{#each events}}
-                            {{> event this}}
-                        {{/each}}
-                </div>
+                {{> events-list this}}
                 {{else}}
                 <div class="loader-wrapper">
                     <img class="loader loader_size_s" src="/img/logo.png">
@@ -91,13 +102,20 @@ export default class SearchBoard {
             </div>
         `;
         this.#parent.innerHTML = window.Handlebars.compile(source)({events: events});
+        this.#addListeners();
     }
 
     #removeListeners() {
+        for (let i = 0;; i++) {
+            const event = <HTMLElement>document.getElementById('event-' + i.toString());
+            if (!event) break;
 
+            event.removeEventListener('click', this.#handleEventClick);
+        }
     }
 
     disable() {
+        this.#removeListeners();
         Bus.off(Events.EventsRes, this.#handleEvents);
         Bus.off(Events.EventsError, this.#handleEventsError);
         this.#parent.innerHTML = '';
