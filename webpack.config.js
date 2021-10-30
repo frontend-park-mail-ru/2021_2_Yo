47f3abbcdad97dbe3515c534b2a5d10f04436688
just4n4cc <just4n4cc@yandex.ru>
2021-10-31 00:18:35 +0300

    temp commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+    context: path.resolve(__dirname, 'public'),
+    mode: 'development',
+    entry: './main.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
+      },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    }
+}
