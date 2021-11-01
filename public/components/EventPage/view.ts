7a437194b26acdde0d42d95fe2c9027c6731cef7
Ksenia <ksju.nikitina@rambler.ru>
2021-11-01 20:58:33 +0300

    запрет на редактирование и удаление мероприятия

--- public/components/EventPage/view.ts
+++ public/components/EventPage/view.ts
@@ -19,24 +19,24 @@ export default class EventPageView {
                     <div class="event-block__event-header event-header">
                         <span>
                             <span class="event-header__event-header-text event-header-text event-header-text_place">
-                                {{city}}
+                                {{event.city}}
                             </span>
                             <span class ="event-header-text"> > </span>
-                            <span class="event-header-text event-header-text_category">{{category}}</span>
+                            <span class="event-header-text event-header-text_category">{{event.category}}</span>
                         </span>
                         <div class="event-block__event-header-views event-header-views">
                             <img class="event-views-img" src="../../server/img/viewedgrey.png">
-                            <span class="event-header-viewed-text">{{viewed}}</span>
+                            <span class="event-header-viewed-text">{{event.viewed}}</span>
                         </div>
                     </div>
         
-                    <p class="event-block__event-title event-title">{{title}}</p>
+                    <p class="event-block__event-title event-title">{{event.title}}</p>
         
-                    <p class="event-block__event-text_header event-text_header">{{description}}</p>
+                    <p class="event-block__event-text_header event-text_header">{{event.description}}</p>
                     
-                    {{#if tag}}
+                    {{#if event.tag}}
                     <span class="event-block__event-tags-block event-tags-block">
-                        {{#each tag}}
+                        {{#each event.tag}}
                             <a class="event-tags-block__event-tag event-tag">{{this}}</a>
                         {{/each}}
                     </span>
@@ -48,19 +48,20 @@ export default class EventPageView {
                 </div>
         
                 <div class="background__event-block event-block">
-                    <p class="event-block__event-text event-text">{{text}}</p>
+                    <p class="event-block__event-text event-text">{{event.text}}</p>
                 </div>
         
                 <div class="background__event-block event-block">
                     <div class="event-block__event-when">
                         <span class="event-text">Когда: </span>
-                        <span class="event-button event-button_orange">{{date}}</span>
+                        <span class="event-button event-button_orange">{{event.date}}</span>
                     </div>
                     <div class="event-block__event-where">
                         <span class="event-text">Где: </span>
-                        <span class="event-button event-button_blue">{{geo}}</span>
+                        <span class="event-button event-button_blue">{{event.geo}}</span>
                     </div>
                 </div>
+                {{#if permission}}
                 <div class="background__event-block event-block">
                     <div class="buttons">
                         <a class="buttons__event-button event-button event-button_blue" id="editButton"">
@@ -71,13 +72,14 @@ export default class EventPageView {
                         </a>
                     </div>
                 </div>
+                {{/if}}
             </div>
         `;
 
         const template = window.Handlebars.compile(source);
 
         const permission = (this.#event.authorid === Userstore.get()?.id);
-        this.#parent.innerHTML = template(this.#event);
+        this.#parent.innerHTML = template({event, permission});
 
         this.#addListeners();
     }
