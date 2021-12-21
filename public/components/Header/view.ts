a95602fa01e796178f2d513aef3566514bf94597
just4n4cc <just4n4cc@yandex.ru>
2021-12-22 01:12:15 +0300

    notifications semi done

--- public/components/Header/view.ts
+++ public/components/Header/view.ts
@@ -4,18 +4,37 @@ import config from '@/config';
 import { Notification, UserData } from '@/types';
 import * as template from '@header/templates/header.hbs';
 import * as notificationSubscribeTemplate from '@header/templates/notificationsubscribe.hbs';
-import * as notificationInviteTemplate from '@header/templates/notificationinvite.hbs';
-import * as notificationCreateTemplate from '@header/templates/notificationcreate.hbs';
+import * as notificationEventTemplate from '@header/templates/notificationevent.hbs';
+// import * as notificationInviteTemplate from '@header/templates/notificationinvite.hbs';
+// import * as notificationCreateTemplate from '@header/templates/notificationcreate.hbs';
 import * as notificationEmptyTemplate from '@header/templates/notificationempty.hbs';
 import '@header/templates/Header.scss';
 import FilterStore, { FilterParams } from '@/modules/filter';
 
-const notificationTemplates = [
-    notificationSubscribeTemplate,
-    notificationInviteTemplate,
-    notificationCreateTemplate,
-    notificationEmptyTemplate,
-];
+// const notificationTemplates = [
+//     notificationSubscribeTemplate,
+//     notificationInviteTemplate,
+//     notificationCreateTemplate,
+//     notificationEmptyTemplate,
+// ];
+
+const notificationsConfig = {
+    0: {
+        template: notificationSubscribeTemplate,
+        text: 'подписался на вас',
+    },
+    1: {
+        template: notificationEventTemplate,
+        text: 'пригласил вас на мероприятия',
+    },
+    2: {
+        template: notificationEventTemplate,
+        text: 'создал мероприятие',
+    },
+    3: {
+        template: notificationEmptyTemplate,
+    }
+};
 
 enum PopupStatus {
     Hidden = -1,
@@ -31,7 +50,6 @@ export default class HeaderView {
     #headerFocusIds = ['header-input', 'header-calendar'];
     #headerClickIds = ['header', 'header-logo'];
     #headerPopups: HTMLElement[] = [];
-    #seen?: boolean;
 
     constructor(parent: HTMLElement) {
         this.#parent = parent;
@@ -76,7 +94,6 @@ export default class HeaderView {
         e.stopPropagation();
         const circle = <HTMLElement>document.getElementById('header-notifications-circle');
         circle.classList.add('hidden');
-        this.#seen = true;
         this.#toggleOverlay(e);
         Bus.emit(Events.UserNotificationsSeen);
     };
@@ -196,7 +213,8 @@ export default class HeaderView {
     #notificationsHandle = (notifications: Notification[]) => {
         this.#headerPopups[0].innerHTML = '';
         if (notifications.length === 0) {
-            this.#headerPopups[0].innerHTML += notificationTemplates[3]();
+            // this.#headerPopups[0].innerHTML += notificationTemplates[3]();
+            this.#headerPopups[0].innerHTML += notificationsConfig['3']['template']();
             return;
         }
         if (!this.#isSeen(notifications)) {
@@ -207,8 +225,14 @@ export default class HeaderView {
             circle.classList.remove('hidden');
         }
         notifications.map(notification => {
-            const type = +notification['type'];
-            this.#headerPopups[0].innerHTML += notificationTemplates[type](notification);
+            // const type = +notification['type'];
+            // this.#headerPopups[0].innerHTML += notificationTemplates[type](notification);
+            const type = <'0'|'1'|'2'>notification['type'];
+            const template = notificationsConfig[type]['template'];
+            this.#headerPopups[0].innerHTML += template({
+                notification: notification, 
+                text: notificationsConfig[type]['text'],
+            });
         });
         this.#notificationsAddListeners();
     };
